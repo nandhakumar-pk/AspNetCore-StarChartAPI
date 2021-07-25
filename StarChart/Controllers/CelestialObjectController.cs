ffef9b072d5ccbabe55721bad0cd481172bd0b4e
nandhakumar-pk <nandha.pk@lntinfotech.com>
2021-07-25 18:06:38 +0530

    Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -15,6 +17,37 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int Id)
+        {
+            var celestialObject = await _context.CelestialObjects.FirstOrDefaultAsync(e => e.Id == Id);
+            if (celestialObject == null)
+                return NotFound();
+            var satellite = await _context.CelestialObjects.FindAsync(celestialObject.Id);
+            if (satellite != null)
+                celestialObject.Satellites.Add(satellite);
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetByName(string name)
+        {
+            var celestialObject = await _context.CelestialObjects.FirstOrDefaultAsync(e => e.Name == name);
+            if (celestialObject == null)
+                return NotFound();
+            var satellite = await _context.CelestialObjects.FindAsync(celestialObject.Id);    
+            if (satellite != null)
+                celestialObject.Satellites.Add(satellite);
+            return Ok(celestialObject);
+        }
+
+        [HttpGet]
+        public void GetAll()
+        {
+           // var celestials = _context.CelestialObjects.ToArrayAsync();
+           // return celestials;
         }
     }
 }
